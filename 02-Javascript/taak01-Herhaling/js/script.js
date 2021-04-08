e92ded4ad187f8f1e04f707aad4415a413acd76e
jakobnakachidze <=>
2021-04-08 15:20:31 +0200

    javascript vchangessssszzz

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,14 @@
 let strNaam;
 let boolNaamKlopt;
+strnaam = prompt("Wat is je naam ?");
+alert("je heet " + strnaam);
+confirm("klopt je naam ? " + strnaam);
+
+if (confirm("klopt je naam ? " + strnaam) == true) {
+    alert ("jaa";)
+}
+else(){
+
+}
+console.log(confirm)
+//safak zij ik mag overslaan
---
+++ 02-Javascript/taak02-Functions-introductie/js/script.js
@@ -0,0 +1,25 @@
+let strnaam
+
+
+function alertbox() {
+    alert ("ewa ziina");
+}
+alertbox()
+
+function prompto() {
+ strnaam = prompt("faaka wats je naam ezebaai ");
+    
+}
+prompto()
+console.log(strnaam)
+
+function gekkeleermethode() {
+    confirm("klik")
+    if (confirm == true) {
+       
+    } else {
+        
+    }
+}
+gekkeleermethode()
+console.log(confirm == true))
