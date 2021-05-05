42f67d7b8e66e82a7781042fa0481d751ddcb7d6
jakobnakachidze <=>
2021-05-05 16:24:05 +0200

    score en hoger lager gemaakt

--- 02-Javascript/taak06-Functions-Samenvatting/js/script.js
+++ 02-Javascript/taak06-Functions-Samenvatting/js/script.js
@@ -1,5 +1,6 @@
 var score = 0;
-let getal1;
+var getal1 = document.querySelector(".getal1").innerHTML;
+var getal2 = document.querySelector(".getal2").innerHTML;
 
 function makeRandomNumber(){
     return Math.floor(Math.random()*99+1);
@@ -11,4 +12,34 @@ function showRandomNumber1(){
     return randomGetal;
 }
 
-getal1 = showRandomNumber1();
+showRandomNumber1()
+
+function showRandomNumber2(){
+    let randomGetal = makeRandomNumber()
+    document.querySelector(".getal2").innerHTML = randomGetal;
+    return randomGetal;
+}
+
+showRandomNumber2()
+
+function scoreplus() {
+    document.querySelector(".score").innerHTML = score + 1;
+}
+
+
+
+function geefAntwoord(input) {
+    if (input == "hoger") {
+        if (getal1 < getal2) {
+        console.log("geen punt")
+            
+        } else {
+            
+        }
+    } else {
+        console.log("wel  punt")
+        scoreplus()
+        }
+
+ }
+
