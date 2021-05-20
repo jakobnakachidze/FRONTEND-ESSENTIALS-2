c4669aad9387604aec058e7feaeab254dea98650
jakobnakachidze <=>
2021-05-20 12:43:53 +0200

    introductieopdracht

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,14 @@
+Toppokoe = ["EetPoenie", "WatWeetJe", "Stacks"];
+console.log(Toppokoe[0]);
+console.log(Toppokoe[1]);
+console.log(Toppokoe[2]);
+
+TopChickie = ["Ezrah", "Anna", "Demi"];
+console.log(TopChickie[0]);
+console.log(TopChickie[1]);
+console.log(TopChickie[2]);
+
+TopWaggie = ["m530i", "M3f30", "m5e50"];
+console.log(TopWaggie[0]);
+console.log(TopWaggie[1]);
+console.log(TopWaggie[2]);
